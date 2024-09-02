fb63d3a8c946e458ff405eb8beba42ee5c5a3b79
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-02 15:34:57 +0300

    updated: variables & constants

--- app/src/main/java/com/ismailmesutmujde/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinlearning/MainActivity.kt
@@ -9,5 +9,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println("hello kotlin")
+
+        // VARIABLES && CONSTANTS
+
+        // Variables (Değişkenler) -> var
+        // var tanımlanan bir değişken sonradan değiştirilebilir.
+
+        var x = 5
+        var y = 4
+        println(x * y)
+
+        var age = 35
+        println(age / 7 * 5)
+
+        age = 36
+        println(age)
+
+        // Constants (Sabitler) -> val
+        // val tanımlanan bir değişken sonradan değiştirilemez.
+        val name = "James"
+        val a = 5
+
     }
 }
